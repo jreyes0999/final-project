961ec3a76f786a4fb0de16cd8e0fbbb36283eb29
Defluxit <astroghost1@gmail.com>
2021-08-16 14:51:08 -0500

    Added additional js files for separate webpages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+function homePage() {
+    // create new element
+    const div = document.createElement('div');
+    div.innerText = "Come try the best wings in Memphis! At J & A's Wings you can never go wrong! Founded in 2021, we strive to deliver the best wings in the city of Memphis!";
+    div.setAttribute("style", "background-color: white;");
+    // append element to body
+    document.body.append(div);
+}
