0b5eae2b29ac54ba6a5d09b7911b9a320a4e1092
Defluxit <astroghost1@gmail.com>
2021-08-17 10:21:15 -0500

    worked on main.js and index.html

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 function homePage() {
-    // create new element
-    const div = document.createElement('div');
-    div.innerText = "Come try the best wings in Memphis! At J & A's Wings you can never go wrong! Founded in 2021, we strive to deliver the best wings in the city of Memphis!";
-    div.setAttribute("style", "background-color: white;");
-    // append element to body
-    document.body.append(div);
+    var div = document.createElement("div"); // create new element
+    div.innerHTML = "At J & A's Wings you can never go wrong! Founded in 2021 by Jonathan and Alston, we strive to deliver the best wings in the city of Memphis! So come on down and try out one of our many famous flavors! Ain't nothin' like some J & A's!"; // adding text on the element
+    document.body.appendChild(div); // appending the element to the body
+}
+homePage(); // calling the function
+
+// when the user clicks the button it hides or shows the dropdown
+function myFunction () {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+// close dropdown menu if clicked out of
+windows.onclick = function (e) {
+    if (!e.target.matches('.dropdownbtn')) {
+        var myDropdown = document.getElementsByClassName("myDropdown");
+        if (myDropdown.classList.contains("show")) {
+            myDropdown.classList.remove("show");
+        }
+    }
 }
